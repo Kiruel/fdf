ae34cf677c618ee7d2f4fd86d93a313372ce251e
Etienne Theodore <etiennetheodore@pc5.home>
2014-12-30 19:27:23 +0100

    go

--- ft_read_data.c
+++ ft_read_data.c
@@ -18,31 +18,51 @@ int		*ft_count_line(char **av)
 	char 	*line;
 	int 	*height;
 	char 	**map_char;
-	int 	i;
+	int 	long_y;
 	int		p;
+	int 	i;
 
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 	{
 		ft_putstr_fd("opent() failed\n", 2);
 	}
+	long_y = 0;
+	while (get_next_line(fd, &line) > 0)
+		long_y++;
+	if (close(fd) == -1)
+	{
+		ft_putstr_fd("close() failed\n", 2);
+		return (0);
+	}	
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("opent() failed\n", 2);
+	}
+	p = 1;
+	height = (int*)ft_memalloc(sizeof(int) * long_y);
 	while (get_next_line(fd, &line) > 0)
 	{
 		map_char = ft_strsplit_fdf(line);
 		i = 0;
 		while (map_char[i])
-			i++;
-		height = (int*)ft_memalloc(sizeof(int) * i);
-		p = 1;
-		i = 0;
-		while (map_char[i])
 		{
-			height[i]++;
-			j++;
+			height[p]++;
+			i++;
 		}
 		height[0]++;
 		p++;
 	}
+	i = 0;
+	get_next_line(fd, &line);
+	map_char = ft_strsplit_fdf(line);
+	while (map_char[i])
+	{
+		ft_putendl(map_char[i]);
+		i++;
+	}
+	ft_putnbr(height[11]);
 	if (close(fd) == -1)
 	{
 		ft_putstr_fd("close() failed\n", 2);
