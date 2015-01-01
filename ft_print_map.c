5caff3aa1d3586ff73b7c8095afcdc6d47c8e61c
Etienne Theodore <etiennetheodore@pc2.home>
2015-01-02 00:12:48 +0100

    go

--- ft_print_map.c
+++ ft_print_map.c
@@ -1,6 +1,22 @@
 #include "includes/fdf.h"
 
-void 	ft_print_segment(t_env *e, int *pts1, int *pts2)
+void 	ft_print_color(t_env *e, int x, int y, int i, int j)
+{
+	if (e->map[i][j]->z <= 0 && e->map[i][j]->z > -80)
+		mlx_pixel_put(e->mlx, e->win, x, y, 0x00B5FF);
+	else if (e->map[i][j]->z <= -80 && e->map[i][j]->z > -160)
+		mlx_pixel_put(e->mlx, e->win, x, y, 0x0061DB);
+	else if (e->map[i][j]->z <= -160)
+		mlx_pixel_put(e->mlx, e->win, x, y, 0x0048A3);
+	else if (e->map[i][j]->z > 0 && e->map[i][j]->z <= 40)
+		mlx_pixel_put(e->mlx, e->win, x, y, 0xF5D37D);
+	else if (e->map[i][j]->z >= 40 && e->map[i][j]->z <= 80)
+		mlx_pixel_put(e->mlx, e->win, x, y, 0x157A0A);
+	else if (e->map[i][j]->z >= 80 && e->map[i][j]->z < 400)
+		mlx_pixel_put(e->mlx, e->win, x, y, 0x523C0B);
+}
+
+void 	ft_print_segment(t_env *e, int *pts1, int *pts2, int i, int j)
 {
 	int *v;
 
@@ -16,7 +32,8 @@ void 	ft_print_segment(t_env *e, int *pts1, int *pts2)
 	v[4] = (v[0] > v[1] ? v[0] : -v[1]) / 2;
 	while (1)
 	{
-		mlx_pixel_put(e->mlx, e->win, v[6], v[8], COLOR);
+		ft_print_color(e, v[6], v[8], i, j);
+		// mlx_pixel_put(e->mlx, e->win, v[6], v[8], COLOR);
 		if ((v[6] == v[7]) && (v[8] == v[9]))
 			break ;
 		v[5] = v[4];
@@ -46,7 +63,7 @@ void	ft_print_segment_right(int i, int j, t_env *e)
 	pts2[0] = (e->map[i][j + 1]->x - e->map[i][j + 1]->y) * e->ecart + 500;
 	pts2[1] = (e->map[i][j + 1]->x + e->map[i][j + 1]->y) * (e->ecart)/2 + 250;
 	pts2[2] = e->map[i][j + 1]->z * e->ecart * e->scale;
-	ft_print_segment(e, pts1, pts2);
+	ft_print_segment(e, pts1, pts2, i, j);
 }
 
 void	ft_print_segment_down(int i, int j, t_env *e)
@@ -54,10 +71,6 @@ void	ft_print_segment_down(int i, int j, t_env *e)
 	int *pts1;
 	int *pts2;
 
-	ft_putnbr(i);
-	ft_putchar(':');
-	ft_putnbr(j);
-	ft_putchar('\n');
 	pts1 = (int*)ft_memalloc(sizeof(int) * 3);
 	pts2 = (int*)ft_memalloc(sizeof(int) * 3);
 	pts1[0] = (e->map[i][j]->x - e->map[i][j]->y) * e->ecart + 500;
@@ -66,7 +79,7 @@ void	ft_print_segment_down(int i, int j, t_env *e)
 	pts2[0] = (e->map[i + 1][j]->x - e->map[i + 1][j]->y) * e->ecart + 500;
 	pts2[1] = (e->map[i + 1][j]->x + e->map[i + 1][j]->y) * (e->ecart)/2 + 250;
 	pts2[2] = e->map[i + 1][j]->z * e->ecart * e->scale;
-	ft_print_segment(e, pts1, pts2);
+	ft_print_segment(e, pts1, pts2, i, j);
 }
 
 /*void	ft_print_map(int i, int j, t_env *e)
@@ -105,9 +118,9 @@ void	draw_map(t_env *e)
 		while (e->map[i][j])
 		{
 			// ft_print_map(i, j, e);
-			// if (e->map[i][j + 1])
-			// 	ft_print_segment_right(i, j, e);
-			if (e->map[i + 1][j] || e->map[i][j + 1])
+			if (e->map[i][j + 1])
+				ft_print_segment_right(i, j, e);
+			if (i < e->size - 1)
 				ft_print_segment_down(i, j, e);
 			j++;
 		}
